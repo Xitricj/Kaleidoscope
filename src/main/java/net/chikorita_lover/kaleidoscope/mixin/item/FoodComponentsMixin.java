cf99cc85c5dc4d44325077892f37e4f57ee46a7c
Xitricj <160381096+Xitricj@users.noreply.github.com>
2025-02-01 14:43:53 +0800

    Added more foods to be eaten quickly

--- src/main/java/net/chikorita_lover/kaleidoscope/mixin/item/FoodComponentsMixin.java
+++ src/main/java/net/chikorita_lover/kaleidoscope/mixin/item/FoodComponentsMixin.java
@@ -11,12 +11,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FoodComponents.class)
 public class FoodComponentsMixin {
-    @Shadow
-    @Final
-    public static FoodComponent COOKIE;
+    @Shadow @Final public static FoodComponent COOKIE;
+    @Shadow @Final public static FoodComponent GLOW_BERRIES;
+    @Shadow @Final public static FoodComponent SWEET_BERRIES;
 
     @Inject(method = "<clinit>", at = @At("TAIL"))
     private static void modifyFoodComponents(CallbackInfo ci) {
         ((FoodComponentAccessor) COOKIE).setSnack(true);
+        ((FoodComponentAccessor) GLOW_BERRIES).setSnack(true);
+        ((FoodComponentAccessor) SWEET_BERRIES).setSnack(true);
     }
 }
